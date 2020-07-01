45dfac568b010dfc80ae9840ece00e81fffd0cd7
maifeeulasad <maifeeulasad@gmail.com>
2020-07-02 00:40:56 +0600

    declare states

--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 class App extends React.Component{
 
+    state={
+        name:"Maifee Ul Aasad",
+        age:2305,
+    };
+
     render() {
         return(
             <div>
